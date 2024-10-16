ead64cadebd976f4c4c6be71fd1e743f6b4448d9
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2024-10-16 19:46:12 +0200

    First commit

---
+++ KotlinFirstContact/src/Main.kt
@@ -0,0 +1,22 @@
+fun main () {
+    print(month(12))
+}
+
+fun month(month: Int): String {
+    val monthStr = return when(month) {
+        1 -> "Enero"
+        2 -> "Febrero"
+        3 -> "Marzo"
+        4 -> "Abril"
+        5 -> "Mayo"
+        6 -> "Junio"
+        7 -> "Julio"
+        8 -> "Agosto"
+        9 -> "Septiembre"
+        10 -> "Octubre"
+        11 -> "Noviembre"
+        12 -> "Diciembre"
+        else -> "Mes inválido"
+    }
+    return monthStr
+}
